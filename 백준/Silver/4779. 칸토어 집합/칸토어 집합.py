aeaac7085e7b55b03dd7dce3e218032ad8629d3a
daey0ung <radio010719@gmail.com>
2026-02-24 22:04:08 +0900

    [Silver III] Title: 칸토어 집합, Time: 36 ms, Memory: 33796 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -0,0 +1,15 @@
+def solve(n):
+    if n==1:
+        return "-"
+    left = solve(n//3)
+    center = " " * (n//3)
+    return left + center + left
+
+while True:
+    try:
+        N = int(input())
+
+        answer = solve(3**N)
+        print(answer)
+    except:
+        break
